95bf68ac4a224fae2bdf833d0c2249f3d1ccd8e7
joaovarelas <joaomvarelas@gmail.com>
2023-11-28 16:57:21 +0000

    picture URL por vars via thymeleaf

--- app1/src/main/java/com/segredes/vulnapp/controller/IndexController.java
+++ app1/src/main/java/com/segredes/vulnapp/controller/IndexController.java
@@ -4,13 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.segredes.vulnapp.auth.UserRepository;
 import com.segredes.vulnapp.config.MvcConfig;
 import com.segredes.vulnapp.model.LoginReq;
+import com.segredes.vulnapp.model.User;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -20,9 +25,19 @@ public class IndexController {
     private static Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/" /* , method = RequestMethod.GET */)
-    public String index(HttpServletResponse request, HttpServletResponse response) {
-
+    public String index(Model model) {
         logger.info("index() - Received request to '/' (index)");
+
+        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //User user = UserRepository.findUser(userDetails.getUsername());
+
+        logger.info("index() - Security context, username: {}", username);
+
+        String profilePicURL = UserRepository.findUser(username).getPicUrl();
+
+        profilePicURL = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngmart.com%2Ffiles%2F11%2FDoge-Meme-PNG-Photos.png&f=1&nofb=1&ipt=d61caf89b1d20322aefc4594b7beb15d013122a923023bd74dabacef10a88194&ipo=images";
+
+        model.addAttribute("profilePicURL", profilePicURL);
 
         return "index";
     }
