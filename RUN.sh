a05c4fbd5526485b3db30d117ed66076333c77bf
joaovarelas <joaomvarelas@gmail.com>
2023-12-05 13:04:29 +0000

    update

--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+# keystore loc, var used on github action
+export KEYSTORE_ALIAS=mykey
+export KEYSTORE_PASSWORD=123456
+export RUNNER_TEMP=`pwd`
+
 # build app1 JAR files
-app1/mvnw -f app1/pom.xml clean package
+app1/mvnw -X -f app1/pom.xml clean package
 
 # build docker containers
 docker-compose up --force-recreate --build
